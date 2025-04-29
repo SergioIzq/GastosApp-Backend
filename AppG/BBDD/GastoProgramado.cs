02676ecad7bd618b4af3c45f8ba6e061910fb60b
Sergio Izquierdo <seizquie@gmail.com>
2025-04-29 12:14:58 +0200

    Cambio entidad gastoProgramado

--- AppG/BBDD/GastoProgramado.cs
+++ AppG/BBDD/GastoProgramado.cs
@@ -22,49 +22,18 @@
             }
         }
 
-        private DateTime _Fecha;
-        public virtual DateTime Fecha
+        private int _DiaEjecucion;
+        public virtual int DiaEjecucion
         {
             get
             {
-                return this._Fecha;
+                return this._DiaEjecucion;
             }
             set
             {
-                if (this._Fecha != value)
+                if (this._DiaEjecucion != value)
                 {
-                    this._Fecha = value;
-                }
-            }
-        }
-
-        private DateTime _FechaInicio;
-        public virtual DateTime FechaInicio
-        {
-            get
-            {
-                return this._FechaInicio;
-            }
-            set
-            {
-                if (this._FechaInicio != value)
-                {
-                    this._FechaInicio = value;
-                }
-            }
-        }
-        private DateTime _FechaFin;
-        public virtual DateTime FechaFin
-        {
-            get
-            {
-                return this._FechaFin;
-            }
-            set
-            {
-                if (this._FechaFin != value)
-                {
-                    this._FechaFin = value;
+                    this._DiaEjecucion = value;
                 }
             }
         }
@@ -90,22 +59,7 @@
                 }
             }
         }
-
-        private string? _Frecuencia;
-        public virtual string? Frecuencia
-        {
-            get
-            {
-                return this._Frecuencia;
-            }
-            set
-            {
-                if (this._Frecuencia != value)
-                {
-                    this._Frecuencia = value;
-                }
-            }
-        }
         public virtual bool Activo { get; set; }
+        public virtual bool AjustarAUltimoDia { get; set; }
     }
 }
