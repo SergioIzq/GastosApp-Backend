b621835564c23b8ae251ccce76c15db0900f15f7
Sergio Izquierdo <seizquie@gmail.com>
2025-10-27 16:32:11 +0100

    Empiezo nueva arquitectura

---
+++ Domain/Abstractions/AbsEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace AppG.Domain.Abstractions;
+
+public abstract class AbsEntity
+{
+    protected AbsEntity()
+    {
+        Id = 0;
+        FechaCreacion = DateTime.Now;
+    }
+
+    private readonly List<IDomainEvent> _domainEvents = new();
+    protected virtual int Id { get; init; }
+    protected virtual DateTime FechaCreacion { get; set; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
